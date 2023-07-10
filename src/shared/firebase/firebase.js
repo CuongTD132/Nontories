5f4a81e6286c5237e0cfb55c92a019491231337b
Duc Cuong <cuongtu532@gmail.com>
2023-07-10 13:35:13 +0700

    add 3 func for admin

--- src/shared/firebase/firebase.js
+++ src/shared/firebase/firebase.js
@@ -7,8 +7,7 @@ import {
     createUserWithEmailAndPassword,
     sendPasswordResetEmail,
     signOut,
-    FacebookAuthProvider,
-    onAuthStateChanged ,
+    onAuthStateChanged,
 } from "firebase/auth";
 import {
     getFirestore,
@@ -21,8 +20,7 @@ import {
 } from "firebase/firestore";
 import {
     getDownloadURL,
-    getMetadata,
-    getStorage, listAll, ref, uploadBytes, uploadBytesResumable
+    getStorage, listAll, ref, uploadBytes, 
 } from "firebase/storage";
 
 const firebaseConfig = {
@@ -51,12 +49,12 @@ const signInWithGoogle = async () => {
     const token = credential.accessToken;
     const user = userCredentials.user;
     console.log(user);
-    
+
     let check = false;
     let photographers = await getAllPhotographers();
     console.log(photographers)
     photographers.forEach((photographer) => {
-        if (photographer.uid == user.uid) check = true;
+        if (photographer.uid === user.uid) check = true;
     })
     let urlRedirect = ""
     if (check) {
@@ -111,7 +109,7 @@ const registerWithEmailAndPassword = async (
         .then(
             (result) => {
                 const user = result.user;
-                if (role == "Thợ chụp ảnh cần tìm kiếm khách hàng") {
+                if (role === "Thợ chụp ảnh cần tìm kiếm khách hàng") {
                     addDoc(collection(db, 'photographer'), {
                         uid: user.uid,
                         name: `${firstName} ${lastName}`,
@@ -183,14 +181,14 @@ const getPhotographer = async (uid) => {
     return photographer;
 }
 
-const createComment = async () => {
-    return await addDoc(collection(db, "comments", {
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        rating: 4,
-        commenter: auth.currentUser.name,
-        locate: "Ho Chi Minh"
-    }));
-}
+// const createComment = async () => {
+//     return await addDoc(collection(db, "comments", {
+//         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//         rating: 4,
+//         commenter: auth.currentUser.name,
+//         locate: "Ho Chi Minh"
+//     }));
+// }
 
 const getAllPhotographers = async () => {
     const querySnapshot = await getDocs(collection(db, "photographer"))
@@ -235,7 +233,7 @@ const updateInforPhotographer = async (
             lastName: lastName,
             accountName: accountName,
             nameInIDCard: nameInIDCard,
-            thumbnailUrl: thumbnailUrl != '' ? thumbnailUrl : `https://ui-avatars.com/api/${firstName}`,
+            thumbnailUrl: thumbnailUrl !== '' ? thumbnailUrl : `https://ui-avatars.com/api/${firstName}`,
             follower: 0,
             rating: 5.0,
             topics: topics,
@@ -295,88 +293,87 @@ const getMultipleImages = async (link) => {
 
 const authStateListener = onAuthStateChanged(auth, (user) => {
     if (user) {
-      console.log("User is signed in:", user);
-      // Perform actions or retrieve user information here
+        console.log("User is signed in:", user);
+        // Perform actions or retrieve user information here
     } else {
-      console.log("User is signed out");
-      // Perform actions when the user is signed out
+        console.log("User is signed out");
+        // Perform actions when the user is signed out
     }
-  });
+});
 
 
-//@param topics : Array<String> topics
-//@returns list of users that have topic is filtered
+//param topics : Array<String> topics
+//returns list of users that have topic is filtered
 const filterTopic = async (topics) => {
     const querySnapshot = await getDocs(collection(db, "photographer"))
     const users = querySnapshot.docs.map(ref => ref.data())
     const usersFiltered = users.filter((user) => {
         const containsAny = topics.some(item => user.topics.includes(item));
-        if(containsAny.length > 0) return user;
+        if (containsAny.length > 0) return user;
     })
     return usersFiltered;
 }
 
 
-//@param salary : number
-//@returns list of users have salary lower than expect salary
+//param salary : number
+//returns list of users have salary lower than expect salary
 const filterSalary = async (salary) => {
     const querySnapshot = await getDocs(collection(db, "photographer"))
     const users = querySnapshot.docs.map(ref => ref.data())
     const usersFiltered = users.filter((user) => {
-        if(user.allowance <= salary) return user;
+        if (user.allowance <= salary) return user;
     })
     return usersFiltered;
 }
 
-//@param workLocation : string
+//param workLocation : string
 //returns return user have workLocation same with search value
 const filterWorkLocation = async (workLocation) => {
     const querySnapshot = await getDocs(collection(db, "photographer"))
     const users = querySnapshot.docs.map(ref => ref.data())
     const usersFiltered = users.filter((user) => {
-        if(user.workLocations == workLocation) return user;
+        if (user.workLocations === workLocation) return user;
     })
     return usersFiltered;
 }
 
-//@param uid : string
-//@returns status of account : true/false
+//param uid : string
+//returns status of account : true/false
 const checkVerified = async (uid) => {
     let photographer;
     const q = query(collection(db, "photographer"));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-        if(photographer.uid == uid) {
+        if (photographer.uid === uid) {
             photographer = doc.data()
         }
     });
     return photographer.verified;
 }
 
-
-//@param uid : string
-//@returns account is in photoPro paid or not : string
+//param uid : string
+//returns account is in photoPro paid or not : string
 const getPhotoLicense = async (uid) => {
     let photographer;
     const q = query(collection(db, "photographer"));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-        if(photographer.uid == uid) {
+        if (photographer.uid === uid) {
             photographer = doc.data()
         }
     });
-    return photographer.pack ?? "Chưa nâng cấp PhotoPro";
+    return photographer.pack ?? "Chưa nâng cấp FotoPro";
 }
 
 
 //param verified : boolean
 //returns updated status
-const updateVerifyStatus = async (verified) => {
+const updateVerifyStatus = async (verified, uid) => {
     const querySnapshot = await getDocs(collection(db, "photographer"));
     let docId;
     querySnapshot.docs
         .forEach((item) => {
-            if (`${item.data().uid} : ${auth.currentUser.uid}`) {
+            if (item.data().uid === uid) {
                 docId = item.id
             }
         });
@@ -385,6 +382,92 @@ const updateVerifyStatus = async (verified) => {
         doc(db, "photographer", docId),
         {
             verified: verified
+        }
+    )
+}
+
+
+//param pack : string
+//returns updated status of pack ('FotoPro or ""').
+ 
+const updatePackStatus = async (pack, uid) => {
+    const querySnapshot = await getDocs(collection(db, "photographer"));
+    let docId;
+    querySnapshot.docs
+        .forEach((item) => {
+            if (item.data().uid === uid) {
+                docId = item.id
+            }
+        });
+
+    return await updateDoc(
+        doc(db, "photographer", docId),
+        {
+            pack: pack
+        }
+    )
+}
+
+const deleteImage = async (uid, topic, images) => {
+    const querySnapshot = await getDocs(collection(db, "photographer"));
+    let docId;
+    const data = querySnapshot.docs
+        .forEach((item) => {
+            if (item.data().uid === uid) {
+                docId = item.id
+            }
+        });
+
+    return await updateDoc(
+        doc(db, "photographer", docId),
+        {
+            images: [
+                data.images.filter((imageSet) => {
+                    return imageSet.topic !== topic
+                }),
+                {
+                    topic: topic,
+                    links: images
+                    /**
+                     * Sample data:
+                     * {
+                     *  topic: "Name of topic",
+                     *  links: [
+                     *     "firestore-path/image1.png",
+                     *     "firestore-path/image2.png",
+                     *  ]
+                     * }
+                     */
+                }
+            ]
+        }
+    )
+}
+
+const deleteTopics = async (uid, topic) => {
+    const querySnapshot = await getDocs(collection(db, "photographer"));
+    let docId;
+    const data = querySnapshot.docs
+        .forEach((item) => {
+            if (item.data().uid === uid) {
+                docId = item.id
+            }
+        });
+
+    
+    return await updateDoc(
+        doc(db, "photographer", docId),
+        {
+            images: [
+                data.images.filter((imageSet) => {
+                    return imageSet.topic !== topic
+                })
+            ],
+            topics: [
+                data.topics.filter((topicName) => {
+                    return topicName !== topic
+                })
+            ]
         }
     )
 }
@@ -413,4 +496,7 @@ export {
     checkVerified,
     getPhotoLicense,
     updateVerifyStatus,
+    updatePackStatus,
+    deleteImage,
+    deleteTopics
 };
